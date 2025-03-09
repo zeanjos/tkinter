47e65f960dbf84abbee5c2ccdf7a463d31921912
zeanjos <pegananjos@gmail.com>
2025-03-09 13:22:14 -0300

    Meu primeiro projeto KKKKKKKKKKKK

--- aprendendotkinter.py
+++ aprendendotkinter.py
@@ -1,17 +1,27 @@
-import requests  # noqa: F401
-from tkinter import * # noqa: F403
-def janelasd():
-    jan = 1
-
+import time as time
+from tkinter import *
 janela = Tk()  # noqa: F405
 janela.title('Multiplicação')
+
+texto_ano = Label(janela, text='Ano atual: ')
+texto_ano.grid(column=2, row=3)
+
+def ano():
+    atual = time.strftime('%Y')
+    texto_ano.config(text=atual)
+
+
 
 
 t_orienta = Label(janela, text="Clique no botão para somar")
 t_orienta.grid(column=0, row=0)
-t_orienta1 = Label(janela, text="Clique aqui agora")
+t_orienta1 = Label(janela, text="Clique no botão para somar")
 t_orienta1.grid(column=3, row=0)
 
-botao = Button(janela, text="Clique aqui", command= janelasd)
+botao = Button(janela, text="Clique aqui para ver o ano atual", command= ano)
 botao.grid(column=2, row=2)
+
+
+
+
 janela.mainloop()

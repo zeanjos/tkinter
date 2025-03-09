91f08282bb9ce44eb17083e1987ec11c50817b6f
zeanjos <pegananjos@gmail.com>
2025-03-09 11:11:49 -0300

    Iniciando

--- Desktop/otherstuto/aprendendotkinter.py
+++ Desktop/otherstuto/aprendendotkinter.py
@@ -6,9 +6,13 @@ num1 = int(input('Digite outro número: '))
 m = num * num1
 print(f'Este é o resultado da multiplicação: {m}')
 
-janela = Tk()
+janela = Tk()  # noqa: F405
 janela.title('Multiplicação')
 janela.attributes('-fullscreen', True)
 
+t_orienta = Label(janela, text="Clique no botão para somar")  # noqa: F405
+t_orienta.grid(column=8, row=0)
 
 janela.mainloop()
+
+

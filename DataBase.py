90e2491b9c7b4129da9e3b78e29304e7e6cf204f
zeanjos <pegananjos@gmail.com>
2025-04-05 08:43:02 -0300

    Criando banco de dados para integrar com python

---
+++ DataBase.py
@@ -0,0 +1,15 @@
+import sqlite3
+conexao = sqlite3.connect('Usuarios.db')
+
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Usuario(
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    User TEXT NOT NULL,
+    Password TEXT NOT NULL
+    
+)
+""")
+
+conexao.close()

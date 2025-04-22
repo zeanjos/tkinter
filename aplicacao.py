115da29edbd0cfbd65a9c2e67d66a966543d8ac6
zeanjos <pegananjos@gmail.com>
2025-04-21 21:47:38 -0300

    Falta integrar database, vou descobrir como faz isso...

--- aplicacao.py
+++ aplicacao.py
@@ -1,38 +1,44 @@
 import customtkinter as ctk
-import DataBase
+import sqlite3 
+database = sqlite3.connect('primeiro_banco.db')
+cursor = database.cursor()
+
+cursor.execute("CREATE TABLE cadastro (usuario text, senha integer, email text, telefone integer)")
+cursor.execute("INSERT INTO cadastro")
 def botao_callback():
+    
     usuario = input_usuario.get()
     senha = input_senha.get()
+
+
 
     if usuario == 'Janjos' and senha == 'jumento123':
         login_correto.configure(text='Login realizado corretamente!', text_color='green')
     else:
         login_correto.configure(text='Login incorreto!', text_color='red')
-            
+
 def registro():
     botao.place_forget()
     checkbox.place_forget()
     login_correto.place_forget()
     cadastro.place_forget()
-    criar_cadastro.place_forget()
-
-    label_email = ctk.CTkLabel(janela, text = 'E-mail', justify = 'center')
-    label_email.place(relx = 0.5, rely = 0.5, anchor = 'center')
-
+    
+    def botao_callback_dois():
+        usuario_cadastro = input_usuario.get()
+        senha_cadastro = input_senha.get()
+        email_cadastro = input_email.get()  
+        
 
     input_email = ctk.CTkEntry(janela, placeholder_text='Digite seu E-mail', justify = 'center')
-    input_email.place(relx = 0.5, rely = 0.6, anchor = 'center')
+    input_email.place(relx = 0.5, rely = 0.4, anchor = 'center')
     pegar_email = input_email.get()
 
-    label_telefone = ctk.CTkLabel(janela, text = 'Telefone de contato', justify = 'center')
-    label_telefone.place(relx = 0.5, rely = 0.7, anchor = 'center')
-
     input_telefone = ctk.CTkEntry(janela, placeholder_text='Digite seu telefone', justify = 'center')
-    input_telefone.place(relx = 0.5, rely = 0.8, anchor = 'center')
+    input_telefone.place(relx = 0.5, rely = 0.5, anchor = 'center')
     pegar_telefone = input_telefone.get()
 
-    criar_cadastro = ctk.CTkButton(janela, text='Criar cadastro', command= registro)
-    criar_cadastro.place(relx = 0.5, rely = 0.9, anchor = 'center')
+    criar_cadastro = ctk.CTkButton(janela, text='Criar cadastro', command=botao_callback_dois)
+    criar_cadastro.place(relx = 0.5, rely = 0.6, anchor = 'center')
 
 ctk.set_appearance_mode('dark')
 janela = ctk.CTk()
@@ -40,38 +46,32 @@ janela.title('Sistema de login')
 janela.geometry('600x600')
 
 
-label_usuario = ctk.CTkLabel(janela, text='Usuario:', justify='center')
-label_usuario.place(relx = 0.5, rely = 0.1, anchor = 'center')
 
 
-input_usuario = ctk.CTkEntry(janela, placeholder_text='Digite seu usuario')
+input_usuario = ctk.CTkEntry(janela, placeholder_text='Digite seu usuario', justify='center')
 input_usuario.place(relx = 0.5, rely = 0.2, anchor = 'center')
 pegar_usuario = input_usuario.get()
 
-label_usuario1 = ctk.CTkLabel(janela, text='Senha:')
-label_usuario1.place(relx = 0.5, rely = 0.3, anchor = 'center')
-
 input_senha = ctk.CTkEntry(janela, placeholder_text='Digite sua senha', justify = 'center', show='*')
-input_senha.place(relx = 0.5, rely = 0.4, anchor = 'center')
+input_senha.place(relx = 0.5, rely = 0.3, anchor = 'center')
 pegar_senha = input_senha.get()
 
 
 checkbox = ctk.CTkCheckBox(janela, text = 'Lembrar login')
-checkbox.place(relx = 0.5, rely = 0.5, anchor = 'center')
+checkbox.place(relx = 0.5, rely = 0.6, anchor = 'center')
 
 
 botao = ctk.CTkButton(janela, text='Entrar', command=botao_callback)
-botao.place(relx = 0.5, rely = 0.6, anchor = 'center')
+botao.place(relx = 0.5, rely = 0.4, anchor = 'center')
 
 
 cadastro = ctk.CTkButton(janela, text='Primeiro acesso', command= registro)
-cadastro.place(relx = 0.5, rely = 0.7, anchor = 'center')
+cadastro.place(relx = 0.5, rely = 0.5, anchor = 'center')
 
 login_correto = ctk.CTkLabel(janela, text='')
-login_correto.place(relx = 0.5, rely = 0.8, anchor = 'center')
+login_correto.place(relx = 0.5, rely = 0.7, anchor = 'center')
 
-criar_cadastro = ctk.CTkButton(janela, text='Criar cadastro', command= registro)
-criar_cadastro.place(relx = 0.5, rely = 0.9, anchor = 'center')
+
 
 janela.mainloop()
 
